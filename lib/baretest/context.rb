a1102d2c2db0ce006d8d32538d3134be839d959b
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2011-01-29 20:06:39 +0100

    Added BareTest::Context.

---
+++ lib/baretest/context.rb
@@ -0,0 +1,21 @@
+# Encoding: utf-8
+
+#--
+# Copyright 2009-2011 by Stefan Rusterholz.
+# All rights reserved.
+# See LICENSE.txt for permissions.
+#++
+
+
+
+module BareTest
+
+  # @author   Stefan Rusterholz <contact@apeiros.me>
+  # @since    0.5.0
+  # @topic    Phases, Components
+  #
+  # An empty class whose instances serve as the execution container for Phases.
+  # Do NOT add methods to this class. Use the component system instead.
+  class Context
+  end
+end
